46b5c4f5d522f98bbed7617c22ee2ea198cfb306
fFukurou <supevi1@gmail.com>
2024-12-18 15:51:04 -0300

    added health bar for base, clicking, tile changing, etc

--- Base/base.gd
+++ Base/base.gd
@@ -1,12 +1,17 @@
 extends Node3D
 
-@export var max_health: int = 5
+@export var MAX_HEALTH: int = 5
+
+var RED: Color = Color.DARK_RED
+var WHITE: Color = Color.WHITE_SMOKE
+var blended_color
 
 var current_health: int:
 	set(health_in):
 		current_health = health_in
 		print("health was changed")
-		label_3d.text = str(current_health)
+		label_3d.text = str(current_health) + "/" + str(MAX_HEALTH)
+		label_3d.modulate = RED.lerp(WHITE, float(current_health) / float(MAX_HEALTH))
 		if current_health < 1:
 			get_tree().reload_current_scene()
 	
@@ -16,7 +21,7 @@ var current_health: int:
 @onready var label_3d: Label3D = $Label3D
 
 func _ready() -> void:
-	current_health = max_health
+	current_health = MAX_HEALTH
 	
 
 func take_damage() -> void:
